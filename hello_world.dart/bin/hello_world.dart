3308f0c8a670dffe3ade177af72f158e193ae7f5
SweetPresent <142983461+guoshaoran@users.noreply.github.com>
2024-04-29 13:18:09 +0000

    1

--- hello_world.dart/bin/hello_world.dart
+++ hello_world.dart/bin/hello_world.dart
@@ -1,48 +1,46 @@
-import 'dart:convert';
+improt 'dart:convert';
 import 'dart:io';
 
 import 'package:args/args.dart';
 
-const lineNumber = 'line-number';
+const lineNumber ='line-number';
 
-void main(List<String> arguments) {
-  exitCode = 0; // Presume success
-  final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
+void main<List<String> arguments>{
+  exitCode=0;
+  final parser=ArgParser()..addFlag(lineNumber,negatable:false,abbe:'n');
 
-  ArgResults argResults = parser.parse(arguments);
-  final paths = argResults.rest;
+  ArgResults argResults=parser.parse(arguments);
+  final paths = argResult.rest;
 
-  dcat(paths, showLineNumbers: argResults[lineNumber] as bool);
+    dcat(paths,showLineNumber:argResult[lineNumber]as bool);
+
 }
+Feture<void> dcat(List<String>paths,{bool showLineNumbers=false})async{
+    if(paths.isEmpty){
+        await stdin.pipe(stdout);
 
-Future<void> dcat(List<String> paths, {bool showLineNumbers = false}) async {
-  if (paths.isEmpty) {
-    // No files provided as arguments. Read from stdin and print each line.
-    await stdin.pipe(stdout);
-  } else {
-    for (final path in paths) {
-      var lineNumber = 1;
-      final lines = utf8.decoder
-          .bind(File(path).openRead())
-          .transform(const LineSplitter());
-      try {
-        await for (final line in lines) {
-          if (showLineNumbers) {
-            stdout.write('${lineNumber++} ');
-          }
-          stdout.writeln(line);
+    }else{
+        for(final path in paths){
+            var lineNumber = 1;
+            final lines = utf8.decoder.bind(File(path).openRead());
+            try{
+                await for(final line in lines){
+                    if(showLineNumbers){
+                        stdout.write('${lineNumber++}');
+                }
+                stdout.writeln(line);
+                }
+            }catch(_){
+                await _handleError(path);
+            }
         }
-      } catch (_) {
-        await _handleError(path);
-      }
     }
-  }
 }
 
-Future<void> _handleError(String path) async {
-  if (await FileSystemEntity.isDirectory(path)) {
-    stderr.writeln('error: $path is a directory');
-  } else {
-    exitCode = 2;
-  }
+Frture<void> _handleError(String path)async{
+    if(await FileSystemEntity.isDirectory(path)){
+        atderr.writeln('error:$path is directory');
+    }else{
+        exitCode=2;
+    }
 }
